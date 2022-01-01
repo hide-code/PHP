7a31485191de983857519bff000d42934b5fc50e
hide-code <kitahide13@icloud.com>
2022-01-01 20:43:02 +0900

    store_phpまで作成、SQLに接続できていない

---
+++ connection.php
@@ -0,0 +1,21 @@
+<?php
+require_once('config.php');
+
+// PDOクラスのインスタンス化
+function connectPdo()
+{
+    try {
+        return new PDO(DSN, DB_USER, DB_PASSWORD);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        exit();
+    }
+}
+
+// 新規作成処理
+function createTodoData($todoText)
+{
+    $dbh = connectPdo();
+    $sql = 'INSERT INTO todos (content) VALUES ("' . $todoText . '")';
+    $dbh->query($sql);
+}
